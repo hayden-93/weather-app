3ad4ccfb8ba07ba05fb71dce63bce6ecd3943a5d
Hayden Buckley-Smith <hayden@abstack.co>
2022-02-08 17:04:06 +0000

    feat: API Endpoints - Added endpoints for weather app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,39 @@
 import type { NextPage } from "next";
 
+import { useQuery } from "react-query";
+
+const WEATHER_API_ENDPOINTS = {
+  current: "http://api.weatherapi.com/v1/current.json",
+  forecast: "http://api.weatherapi.com/v1/forecast.json",
+  search: "http://api.weatherapi.com/v1/search.json",
+  history: "http://api.weatherapi.com/v1/history.json",
+  timezone: "http://api.weatherapi.com/v1/timezone.json",
+  sports: "http://api.weatherapi.com/v1/sports.json",
+  astronomy: "http://api.weatherapi.com/v1/astronomy.json",
+  ip: "http://api.weatherapi.com/v1/ip.json",
+};
+
+const fetchSports = async () => {
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+
+  if (!apiKey) {
+    throw new Error("Missing API key");
+  }
+
+  const url = `${WEATHER_API_ENDPOINTS.sports}?key=${apiKey}&q=London`;
+
+  return fetch(url).then((response) => response.json());
+};
+
 const Home: NextPage = () => {
+  const { status, data } = useQuery("sports", fetchSports);
+
   return (
-    <>
-      <h1>Weather App</h1>
-    </>
+    <div>
+      <pre>
+        <code>{JSON.stringify(data, null, 2)}</code>
+      </pre>
+    </div>
   );
 };
 
